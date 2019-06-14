b81895d86632335a762d31501fa2e9ed96ab2a87
zengqiangrui <zengqiangrui@163.com>
2019-06-14 10:07:21 +0800

    nodejs

--- unpackage/dist/dev/app-plus/app-view.js
+++ unpackage/dist/dev/app-plus/app-view.js
@@ -1271,7 +1271,7 @@ __wxAppCode__['pages/person/person.wxml']=$gwx('./pages/person/person.wxml');
 __wxAppCode__['pages/rank/rank.wxss']=undefined;    
 __wxAppCode__['pages/rank/rank.wxml']=$gwx('./pages/rank/rank.wxml');
 
-__wxAppCode__['pages/trade/trade.wxss']=undefined;    
+__wxAppCode__['pages/trade/trade.wxss']=setCssToHead([".",[1],"content { height: ",[0,100],"; background: darkgrey; color: white; display: -webkit-box; display: -webkit-flex; display: -ms-flexbox; display: flex; -webkit-box-align: center; -webkit-align-items: center; -ms-flex-align: center; align-items: center; -webkit-box-pack: end; -webkit-justify-content: flex-end; -ms-flex-pack: end; justify-content: flex-end; font-size: ",[0,12],"; }\n.",[1],"asset-area { width: 100%; height: ",[0,200],"; }\n",],undefined,{path:"./pages/trade/trade.wxss"});    
 __wxAppCode__['pages/trade/trade.wxml']=$gwx('./pages/trade/trade.wxml');
 
 ;var __pageFrameEndTime__ = Date.now();
